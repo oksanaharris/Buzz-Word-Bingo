1514666db171520f5911d60e8d4a5e893b2e114e
Oksana Harris <oksanaharris@Oksanas-MacBook-Pro.local>
2017-07-19 15:14:42 -1000

    finished

--- app.js
+++ app.js
@@ -2,16 +2,28 @@ var express = require('express');
 var app = express();
 var bodyParser = require('body-parser');
 
+
 var server = app.listen(3000, () => {
   console.log('Server listening.');
 });
 
 
-
-
 class BuzzWordArray {
   constructor (){
     this.buzzWords =[]
+  }
+
+  find(word){
+    var found = false;
+    this.buzzWords.forEach((wordObj, index, array) => {
+      if (wordObj.buzzWord === word){
+        console.log('found a match:', wordObj.buzzWord, word);
+        console.log('find returning the wordObj', wordObj);
+        found = wordObj;
+      }
+    });
+
+    return found;
   }
 
   delete(buzzWord){
@@ -27,13 +39,20 @@ class BuzzWordArray {
     });
     return found;
   }
+
+  reset(){
+    this.buzzWords = [];
+  }
 }
 
+
 var buzzWords = new BuzzWordArray();
+
 
 var player1 = {
   score: 0
 }
+
 
 class BuzzWord {
   constructor (buzzWord, points){
@@ -46,67 +65,60 @@ class BuzzWord {
     buzzWords.buzzWords.push(this);
   }
 
-  // destroy(buzzword){
-  //   let found = false;
-  //   buzzWords.forEach((wordObj, index, array) => {
-  //     if (wordObj.buzzWord === buzzword){
-  //       console.log('found a match:', wordObj.buzzWord, req.params.buzzword);
-  //       array.splice(index, 1);
-  //       found = true;
-  //     }
-  //   });
-  //   return found;
-  // }
+  incrementHeard(){
+    this.heard++;
+  }
 }
 
 
 app.use(express.static('public'));
 
+
 app.use(bodyParser.urlencoded({ extended: false }));
 
+
 app.use(bodyParser.json());
+
 
 app.get('/buzzwords', function (req, res) {
   res.send(JSON.stringify({buzzWords}));
 });
 
-app.post('/buzzword', function (req, res) {
 
+app.post('/buzzword', function (req, res) {
   console.log('REQUEST BODY');
   console.log(req.body);
-
-  // add validation that it's not an existing word in our stored array
-  // should not be able to post to an existing word
-
-  let newWord = new BuzzWord(req.body.buzzWord, parseInt(req.body.points));
-  newWord.save();
+  //not allowing to post to an existing word
+  if (!buzzWords.find(req.body.buzzWord)){
+    let newWord = new BuzzWord(req.body.buzzWord, parseInt(req.body.points));
+    newWord.save();
+    res.send(JSON.stringify({ "success": true }));
+  } else {
+    res.send(JSON.stringify({ "success": false, "error": 'tried to post to an existing word.'}));
+  }
   console.log(buzzWords);
-  res.send(JSON.stringify({ "success": true }));
 });
+
 
 app.put('/:buzzword', function (req, res) {
-  console.log(req.body.heard);
-  //validation if this leads to a valid word stored in our array?
-  buzzWords.forEach((wordObj) => {
-    console.log('running the forEach function');
-    if(wordObj.buzzWord === req.body.buzzWord){
-      //additional validation here if the buzzWord key-value passed in matches the url?
-      console.log('wordObj.buzzWord', wordObj.buzzWord);
-      console.log('req.body.buzzWord', req.body.buzzWord);
-      console.log('that means its found a match');
-      if(req.body.heard === 'true' || req.body.heard === true){
-        // wordObj.heard = parseInt(wordObj.heard);
-        wordObj.heard++;
-        console.log('wordObj.heard', wordObj.heard);
-        player1.score += wordObj.points;
-        console.log('new score', player1.score);
-      }
+  //not doing any validation to make sure buzzWord key value matches url entry - going off of url
+  let wordInQuestion = buzzWords.find(req.params.buzzword);
+  console.log('printing what find returns', wordInQuestion);
+  if (wordInQuestion){
+    if (req.body.heard === 'true' || req.body.heard === true){
+      wordInQuestion.incrementHeard();
+      player1.score += wordInQuestion.points;
+      console.log('new score', player1.score);
+      res.send(JSON.stringify({ "success": true, "newScore": player1.score}));
+    } else {
+      res.send(JSON.stringify({ "success": true, "anything happen?": "not really"}));
     }
-  });
+  } else {
+    res.send(JSON.stringify({ "success": false, "error": 'tried to edit a word that isn\'t in there.'}));
+  }
   console.log(buzzWords);
-  res.send(JSON.stringify({ "success": true, "newScore": player1.score}));
-  //how do you return false? how do you check if "success"?
 });
+
 
 app.delete('/:buzzword', function (req, res) {
   console.log('trying to delete', req.body.buzzWord, req.params.buzzword);
@@ -120,8 +132,15 @@ app.delete('/:buzzword', function (req, res) {
   }
 });
 
+
 app.post('/reset', function (req, res) {
   if (req.body.reset === 'true' || req.body.reset === true){
-
+    buzzWords.reset();
+    player1.score = 0;
+    res.send(JSON.stringify({ "success": true }));
+  } else {
+    res.send(JSON.stringify({ "success": true, "anything happen?": "not really"}));
   }
+  console.log(buzzWords);
+  console.log(player1);
 });
